fc1ebaaf3dfc68424457c8ba08af31b2aa9a2662
Oyvind Odegard <whirlwin90@gmail.com>
2012-05-09 13:26:19 +0200

    Fixed Visual Studio.

--- www/javascripts/common/application.js
+++ www/javascripts/common/application.js
@@ -1,20 +1,13 @@
 ﻿student = {};
 
-htmlEncode = function(s) {
-encodedHtml = escape(s);
-encodedHtml = encodedHtml.replace(/%20/g," ");
-encodedHtml = encodedHtml.replace(/\//g,"%2F");
-encodedHtml = encodedHtml.replace(/\?/g,"%3F");
-encodedHtml = encodedHtml.replace(/=/g,"%3D");
-encodedHtml = encodedHtml.replace(/&/g,"%26");
-encodedHtml = encodedHtml.replace(/@/g,"%40");
-return encodedHtml;
-}
-
 showErr = function(err, alertDismissed) {
-navigator.notification.alert(err, alertDismissed, 'Error', 'OK');
+  navigator.notification.alert(err, alertDismissed, 'Error', 'OK');
 }
 
 showMsg = function(msg, alertDismissed) {
-navigator.notification.alert(msg, alertDismissed, 'Info', 'OK');
+  navigator.notification.alert(msg, alertDismissed, 'Info', 'OK');
+}
+
+checkValue = function(val) {
+  return (val == null ? false : (val == '' ? false : (typeof (val) == 'undefined') ? false : true)); 
 }
