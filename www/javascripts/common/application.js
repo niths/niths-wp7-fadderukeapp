372f1100d6b36a08406a6067cbc78c7cc91e059f
Oyvind Odegard <whirlwin90@gmail.com>
2012-05-09 12:37:02 +0200

    Fixed application.js.

---
+++ www/javascripts/common/application.js
@@ -0,0 +1,20 @@
+﻿student = {};
+
+htmlEncode = function(s) {
+encodedHtml = escape(s);
+encodedHtml = encodedHtml.replace(/%20/g," ");
+encodedHtml = encodedHtml.replace(/\//g,"%2F");
+encodedHtml = encodedHtml.replace(/\?/g,"%3F");
+encodedHtml = encodedHtml.replace(/=/g,"%3D");
+encodedHtml = encodedHtml.replace(/&/g,"%26");
+encodedHtml = encodedHtml.replace(/@/g,"%40");
+return encodedHtml;
+}
+
+showErr = function(err, alertDismissed) {
+navigator.notification.alert(err, alertDismissed, 'Error', 'OK');
+}
+
+showMsg = function(msg, alertDismissed) {
+navigator.notification.alert(msg, alertDismissed, 'Info', 'OK');
+}
