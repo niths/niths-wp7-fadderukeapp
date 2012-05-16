717bfae45bbf3afdbed5f10eaf7e741ece4f3967
Oyvind Odegard <whirlwin90@gmail.com>
2012-05-16 10:15:55 +0200

    Added back button functionality.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,16 +13,36 @@ using Microsoft.Phone.Controls;
 using System.IO;
 using System.Windows.Media.Imaging;
 using System.Windows.Resources;
+using Niths.Util;
+using System.Windows.Navigation;
+
 
 
 namespace niths_wp7_fadderuke_app
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private WebBrowserHelper _browserHelper;
+
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+
+            new BackButtonHandler(this, PGView);
+            _browserHelper = new WebBrowserHelper(PGView.Browser);
+
+            PGView.Browser.ScriptNotify += Browser_ScriptNotify;
+            PGView.Browser.Navigated += Browser_Navigated;
+
+            EventHandler<NavigationEventArgs> hideSplashScreen = null;
+            hideSplashScreen = (s, e) =>
+            {
+                fadeOut.Begin();
+                PGView.Browser.Navigated -= hideSplashScreen;
+            };
+            PGView.Browser.Navigated += hideSplashScreen;
         }
 
         private void GapBrowser_Loaded(object sender, RoutedEventArgs e)
@@ -33,6 +53,22 @@ namespace niths_wp7_fadderuke_app
 	private void fadeOut_Completed(object sender, EventArgs e) {
         //splashImage.Visibility = Visibility.Collapsed;
 	}
+
+  private void Browser_Navigated(object sender, NavigationEventArgs e)
+  {
+      // when we first navigate to a page, we assume that it can be scrolled
+      _browserHelper.ScrollDisabled = false;
+  }
+
+  private void Browser_ScriptNotify(object sender, NotifyEventArgs e)
+  {
+      // if a page notifies that it should not be scrollable, disable
+      // scrolling.
+      if (e.Value == "noScroll")
+      {
+          _browserHelper.ScrollDisabled = true;
+      }
+  }
 
     private void PGView_Loaded(object sender, RoutedEventArgs e)
     {
---
+++ Plugins/BackButtonHandler.cs
@@ -0,0 +1,41 @@
+﻿using WP7CordovaClassLib.Cordova;
+using Microsoft.Phone.Controls;
+using System.ComponentModel;
+
+public class BackButtonHandler
+{
+    private int _browserHistoryLength = 0;
+    //private PGView _phoneGapView;
+
+    private WP7CordovaClassLib.CordovaView _phoneGapView;
+
+    public BackButtonHandler(PhoneApplicationPage page, WP7CordovaClassLib.CordovaView phoneGapView)
+    {
+        // subscribe to the hardware back-button
+        page.BackKeyPress += Page_BackKeyPress;
+
+        // handle navigation events
+        phoneGapView.Browser.Navigated += Browser_Navigated;
+
+        _phoneGapView = phoneGapView;
+    }
+
+    private void Browser_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+    {
+        if (e.NavigationMode == System.Windows.Navigation.NavigationMode.New)
+        {
+            _browserHistoryLength++;
+        }
+    }
+
+    private void Page_BackKeyPress(object sender, CancelEventArgs e)
+    {
+        if (_browserHistoryLength > 1)
+        {
+            _phoneGapView.Browser.InvokeScript("eval", "history.go(-1)");
+            // 2 was changed to 1
+            _browserHistoryLength -= 1;
+            e.Cancel = true;
+        }
+    }
+}
