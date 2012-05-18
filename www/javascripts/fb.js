c4f70d31b4be65f060182ff468c68571625bdd01
Oyvind Odegard <whirlwin90@gmail.com>
2012-05-18 10:55:40 +0200

    Using the APIs URLs.

--- www/javascripts/fb.js
+++ www/javascripts/fb.js
@@ -39,7 +39,25 @@ $('#Gallery1').live('pageshow', function() {
    * 
    * Open browser, go to facebook, select album, copy paste id from url
    */
-  function getLatestImages(){
+  function getLatestImages() { 
+    $.get(
+        'http://ec2-46-137-46-84.eu-west-1.compute.amazonaws.com:8080/niths/sociallinks?socialCommunity=facebook&category=fadderuka',
+        function(socialLinks) {
+          $.ajax({
+            url:      socialLinks[0].address,
+            dataType: 'json',
+            type:     'GET',
+            timeout:  5000,
+            success:  function(album) {
+              handleImgsData(album.data);
+            },
+            error:    function() {
+              showErr('Ikke kontakt med facebook', null);
+            }
+          });
+        }
+    );
+
     $.ajax({
       url:      'https://graph.facebook.com/10150304760787369/photos',
       dataType: 'json',
